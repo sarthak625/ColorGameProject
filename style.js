4969f216c5ce55c7390d83d935061d2331921988
Sarthak <sarthakn18@gmail.com>
2017-06-12 17:07:52 +0530

    Added event listeners to the squares

--- style.js
+++ style.js
@@ -8,5 +8,19 @@ var colors = ["rgb(255, 0, 0)",
 var squares = document.querySelectorAll('.square');
 
 for (var i=0;i<squares.length;i++){
+	//Add initial colors to squares
 	squares[i].style.backgroundColor = colors[i];
+
+	//Add click listener to squares
+	squares[i].addEventListener("click",function(){
+		//Grab color of picked square and compare to original color
+		if (this.style.backgroundColor === pickedColor)
+		{
+			alert("Correct");
+		}
+	})
 }
+var pickedColor = colors[3];
+
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
